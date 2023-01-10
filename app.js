49410c676663240f8322766f27da561f6a52f4b9
ZHONGZHENJIE <508916879@qq.com>
2023-01-10 15:27:17 +0800

    更新

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
 var indexRouter = require('./routes/index');
 var GetTokenRouter = require('./routes/Get_Token');
 var GetMusicInfoRouter = require('./routes/Get_Music_Info');
 
-var app = express();
-
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,19 +23,11 @@ app.use('/', indexRouter);
 app.use('/Get_Token', GetTokenRouter);
 app.use('/Music_Info', GetMusicInfoRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
 
